581003cd37ebd243c258b4540372851943254933
LibCorner <1027721843@qq.com>
2016-07-30 19:47:01 +0800

    add

---
+++ branch/full_connected_feed.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding=utf-8
+import tensorflow as tf
+
+learning_rate=0.01
+max_steps=2000
+hidden1_unit=128
+hidden2_unit=32
+batch_size=100
+train_dir='mnist-data'
+fake_data=False
+
+
+import mnist
+import mnist_data as input_data
+import time
+
+def placeholder_inputs(batch_size):
+    image_placeholder = tf.placeholder(tf.float32, shape=[batch_size,mnist.IMAGE_PIXELS])
+    label_placeholder = tf.placeholder(tf.int32, shape=batch_size)
+    return image_placeholder,label_placeholder
+
+def fill_placeholder(data_set,image_pl,labels_pl):
+    """返回tensorflow的feed字典"""
+    image_feed,label_feed=data_set.next_batch(batch_size)
+    feed_dict={image_pl:image_feed,labels_pl:label_feed}
+    return feed_dict
+
+def do_eval(sess,eval_correct,image_placeholder,label_placeholder,dataset):
+    true_count=0
+    steps_per_epoch = dataset.num_examples // batch_size
+    num_examples=steps_per_epoch * batch_size
+
+    for step in xrange(steps_per_epoch):
+        feed_dict=fill_placeholder(dataset,image_placeholder,label_placeholder)
+        true_count+=sess.run(eval_correct,feed_dict=feed_dict)
+
+    precision=1.0 * true_count / num_examples
+    print('Number of examples: %d Num correct: %d Precision: @ 1: %.4f' %(num_examples,true_count,precision))
+
+def run_training():
+    data_sets=input_data.read_data_sets(fake_data)
+    
+
+    with tf.Graph().as_default():
+        image_placeholder,label_placeholder = placeholder_inputs(batch_size)
+
+        logits=mnist.inference(image_placeholder,hidden1_unit,hidden2_unit)
+
+        loss=mnist.loss(logits,label_placeholder)
+
+        train_op=mnist.training(loss,0.01)
+
+        eval_correct=mnist.evaluation(logits,label_placeholder)
+
+        summary_op=tf.merge_all_summaries()
+
+        saver=tf.train.Saver()
+
+        sess=tf.Session()
+
+        sess.run(tf.initialize_all_variables())
+
+        summary_writter=tf.train.SummaryWriter(train_dir,sess.graph)
+
+        for step in xrange(max_steps):
+            start_time=time.time()
+            
+            feed_dict=fill_placeholder(data_sets.train,image_placeholder,label_placeholder)
+
+            _,loss_value=sess.run([train_op,loss],feed_dict=feed_dict)
+
+            duration = time.time()-start_time
+
+            if step % 100==0:
+                print('Step %d:loss=%.2f (%.3f sec) %(step,loss_value,duration)')
+
+                summary_str=sess.run(summary_op,feed_dict=feed_dict)
+                summary_writter.add_summary(summary_str,step)
+                summary_writter.flush()
+
+            if step % 1000==0:
+                saver.save(sess,train_dir,global_step=step)
+                
+                print('Training Data Eval:')
+                do_eval(sess,eval_correct,image_placeholder,label_placeholder,data_sets.train)
+
+                print("Validation Data Eval")
+                do_eval(sess,eval_correct,image_placeholder,label_placeholder,data_sets.validation)
+
+                print("Test Data Eval:")
+                do_eval(sess,eval_correct,image_placeholder,label_placeholder,data_sets.test)
+
+
+
+if __name__=="__main__":
+    print("runing...")
+    run_training()
