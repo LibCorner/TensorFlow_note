306e6c2e829f4eb97e055d3007bc41f9301da804
LibCorner <1027721843@qq.com>
2016-08-03 11:17:43 +0800

    add MLP

---
+++ branch/examples/MLP/mnist.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding=utf-8
+import tensorflow as tf
+import math
+
+NUM_CLASSES=10
+IMAGE_SIZE=28
+IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
+
+def inference(image,hidden1_unit,hidden2_unit):
+    '''
+    参数：
+        image: 输入的图像
+        hidden1_unit: 第一个隐藏层的神经元个数
+        hidden2_unit:第二个隐藏层的神经元个数
+    返回：
+        输出结果
+    '''
+    #定义每一层的参数和计算过程
+    with tf.name_scope("hidden1"):
+        w=tf.Variable(tf.truncated_normal([IMAGE_PIXELS,hidden1_unit], stddev=1.0/math.sqrt(IMAGE_PIXELS)),name='w')
+        b=tf.Variable(tf.zeros([hidden1_unit]))
+        hidden1=tf.nn.relu(tf.matmul(image,w)+b)
+    
+    with tf.name_scope("hidden2"):
+        w=tf.Variable(tf.truncated_normal([hidden1_unit,hidden2_unit],stddev=1.0/math.sqrt(hidden1_unit)),name='w')
+        b=tf.Variable(tf.zeros([hidden2_unit]))
+        hidden2=tf.nn.relu(tf.matmul(hidden1,w)+b)
+
+    with tf.name_scope("softmax_linear"):
+        w=tf.Variable(tf.truncated_normal([hidden2_unit,NUM_CLASSES],stddev=1.0/math.sqrt(hidden2_unit)),name='w')
+        b=tf.Variable(tf.zeros([NUM_CLASSES]))
+        logits=tf.matmul(hidden2,w)+b
+        logits=tf.nn.softmax(logits)
+
+    return logits
+
+def loss(logits,labels):
+    """定义损失函数"""
+    cross_entropy=tf.nn.sparse_softmax_cross_entropy_with_logits(logits,labels,name='xentropy')
+    loss=tf.reduce_mean(cross_entropy,name='xentropy-mean')
+    return loss
+
+def training(loss,learning_rate):
+    tf.scalar_summary(loss.op.name,loss)
+    #优化器
+    optimizer=tf.train.GradientDescentOptimizer(learning_rate)
+    global_step=tf.Variable(0,name='global_step',trainable=False)
+    train_op=optimizer.minimize(loss,global_step=global_step)
+
+    return train_op
+
+def evaluation(logits,labels):
+    '''计算正确分类数'''
+    correct=tf.nn.in_top_k(logits,labels,1)
+    return tf.reduce_sum(tf.cast(correct,tf.int32))
---
+++ branch/examples/MLP/mnist_data.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+#minist_data
+import tensorflow as tf
+import os
+import gzip
+from six.moves import urllib
+import cPickle as pickle
+import numpy as np
+import pylab
+
+def load_data(dataset='mnist.pkl.gz'):
+    new_path=os.path.join(os.path.split(__file__)[0],dataset)
+    if not os.path.isfile(new_path):
+        origin='http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+        print('Downloading data from %s' %origin)
+        urllib.requestion.urlretrieve(origin,new_path)
+    #加载数据
+    with gzip.open(new_path) as f:
+        train_set,valid_set,test_set=pickle.load(f)
+    return train_set,valid_set,test_set
+    
+def dense_to_one_hot(labels_desnse,num_calsses=10):
+    '''把scalar的类标签转换成one-hot向量'''
+    num_labels=labels_desnse.shape[0]
+    labels_one_hot=np.zeros((num_labels,num_calsses),dtype="float32")
+    labels_one_hot[np.arange(num_labels),labels_desnse]=1.0
+    return labels_one_hot
+class DataSet(object):
+    def __init__(self,images,labels,fake_data=False):
+        if fake_data:
+            self._num_examples=10000
+        else:
+            assert images.shape[0]==labels.shape[0],(
+                "image.shape:%s labels.shape: %s" % (images.shape,labels.shape)
+                 )
+            self._num_examples=images.shape[0]
+            #shape从[num examples, rows, columns, depth]
+            #转为[num examples, rows*columns] (假设depth=1)
+            assert images.shape[3]==1
+            images=images.reshape(images.shape[0],images.shape[1]*images.shape[2])
+            #从[0,255]转换为[0,1.0]
+            images=images.astype(np.float32)
+            images=np.multiply(images,1.0/255.0)
+        self._images=images
+        self._labels=labels
+        self._epochs_completed=0
+        self._index_in_epoch=0
+    @property
+    def images(self):
+        return self._images
+    @property
+    def labels(self):
+        return self._labels
+    @property
+    def num_examples(self):
+        return self._num_examples
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+    def next_batch(self,batch_size,fake_data=False):
+        if fake_data:
+            fake_image=[1.0 for _ in range(784)]
+            fake_label=0
+            return [fake_image for _ in xrange(batch_size)],[fake_label for _  in xrange(batch_size)]
+        start=self._index_in_epoch
+        self._index_in_epoch+=batch_size
+        if self._index_in_epoch >self._num_examples:
+            #结束epoch
+            self._epochs_completed+=1
+            #Shuffle the data
+            perm=np.arange(self._num_examples)
+            np.random.shuffle(perm)
+            self._images=self._images[perm]
+            self._labels=self._labels[perm]
+            #开始下一个epoch
+            start=0
+            self._index_in_epoch=batch_size
+            assert batch_size<=self._num_examples
+        end=self._index_in_epoch
+        return self._images[start:end],self._labels[start:end]
+
+def read_data_sets(fake_data=False,one_hot=False):
+    class DataSets(object):
+        pass
+    data_sets=DataSets()
+    if fake_data:
+        data_sets.train=DataSet([],[],fake_data=True)
+        data_sets.validation=DataSet([],[],fake_data=True)
+        data_sets.test=DataSet([],[],fake_data=True)
+        return data_sets
+    train_data,validation_data,test_data=load_data()
+    train_images,train_labels=train_data
+    train_images=np.asarray(np.reshape(train_images,(train_images.shape[0],28,28,1)),dtype='float32')
+    if one_hot:
+        train_labels=dense_to_one_hot(train_labels)
+
+    validation_images,validation_labels=validation_data
+    validation_images=np.asarray(np.reshape(validation_images,(validation_images.shape[0],28,28,1)),dtype="float32")
+    if one_hot:
+         validation_labels=dense_to_one_hot(validation_labels)
+
+    test_images,test_labels=test_data
+    test_images=np.asarray(np.reshape(test_images,(test_images.shape[0],28,28,1)),dtype="float32")
+    if one_hot:
+        test_labels=dense_to_one_hot(test_labels)
+
+    data_sets.train=DataSet(train_images,train_labels)
+    data_sets.validation=DataSet(validation_images,validation_labels)
+    data_sets.test=DataSet(test_images,test_labels)
+    return data_sets
+
+
+
+
+
+if __name__=="__main__":
+    train_set,valid_set,test_set=load_data()
+    x=train_set[0]
+    y=train_set[1]
+    x=np.asarray(np.reshape(x,(x.shape[0],28,28,1)),dtype="float32")
+    one_hot=dense_to_one_hot(y)
+    print(x.shape)
+    
+    data_sets=read_data_sets(one_hot=True)
+    train=data_sets.train
+    data,label=train.next_batch(20)
+    
+    pylab.imshow(np.reshape(x[10],(28,28)))
+    
+
